8f52d8540113596bf4f0e2f46e7d16dc7c415b1d
Anastasiya <na_sty191@mail.ru>
2018-03-01 21:51:36 +0700

    update

--- server.cpp
+++ server.cpp
@@ -1,11 +1,12 @@
 #include <mpi.h>
-#include "task.cpp"
+#define HAVE_STRUCT_TIMESPEC
 #include <pthread.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <iostream>
 #include <fstream>
 #include <queue>
+#include <vector>
 #define MAX_DATA 1000
 
 #define ERROR_CREATE_THREAD   -11
@@ -16,169 +17,310 @@
 #define ERROR_DESTROY_BARRIER -16
 #define SUCCESS                 0
 
+// Размер задачи
+const int globalTaskSize = 100;
+// Количество задач на один процесс
+int tasksPerProcess = 100;
+// Векторы, которые предстоит умножить
+int v1[globalTaskSize], v2[globalTaskSize];
 
-int ids[6] = {0,1,2,3,4,5};
-//четрыре объекта типа "описатель потока"
-pthread_t thrs[3];
+int globalRes = 0, resultOfGlobalTask = 0;
+
+int ids[7] = { 0,1,2,3,4,5,6 };
+
+pthread_t thrs[4];
 pthread_barrier_t barrier;
-int rank, size;
-int condition = 0;
+int rank, rank_old = 0, size, size_old = 0;
+int count_change = 0;
+
+// Текущий коммуникатор
 MPI_Comm currentComm = MPI_COMM_WORLD;
+// Новый коммуникатор
 MPI_Comm newComm;
+
+int countOfWorkers = 4;
+int countOfThreads = 6;
+
+int condition = 0;
 bool changeComm = false;
 bool serverFinished = false;
-int countOfWorkers = 1;
-int countOfThreads = 3;
-std::queue<Task*> tasksContainer;
+
+pthread_mutex_t mutex;
+
+class Task
+{
+	// Начало и конец обработки вектора
+	int beg, end;
+public:
+	Task(int b, int e) :beg(b), end(e) {}
+	void Run();
+	// Начало подзадачи
+	int GetBegin() { return beg; }
+	// Конец подзадачи
+	int GetEnd() { return end; }
+};
+
+void Task::Run()
+{
+	int res = 0;
+	for (int i = beg; i < end; i++)
+		res += v1[i] * v2[i];
+	// захват мьютекса
+	pthread_mutex_lock(&mutex);
+	// добавление к глобальному результату при исключительном владении глобальной переменной
+	globalRes += res;
+	// освобождение мьютекса
+	pthread_mutex_unlock(&mutex);
+}
+
+std::queue<Task*> QueueOfTasks;
 
 bool GetTask(Task **currTask)
 {
-	//{
-        //std::lock_guard<std::mutex> mylock(mutex);
 	pthread_mutex_lock(&mutex);
-	if(tasksContainer.empty())
-        {
+
+	if (QueueOfTasks.empty())
+	{
 		pthread_mutex_unlock(&mutex);
 		return false;
 	}
-        else
-        {
-        	*currTask = tasksContainer.front();
-		tasksContainer.pop();
-        }
- 	pthread_mutex_unlock(&mutex);
-	//}
+	else
+	{
+		*currTask = QueueOfTasks.front();
+		QueueOfTasks.pop();
+	}
+
+	pthread_mutex_unlock(&mutex);
 	return true;
 }
 
-void ChangeCommunicator()
-{
-        MPI_Barrier(currentComm);
-        pthread_barrier_wait(&barrier);
-        condition = 1;
-        MPI_Send(&condition, 1, MPI_INT, rank, 1, currentComm);
-        while(changeComm);
-}
-
-//функция потока
+// Функция вычислительного потока
 void* worker(void* me)
 {
 	Task *currTask;
+	int  exitTask = 0;
+	bool message = false;
+	MPI_Comm Comm = currentComm;
 
 	// Пока есть свои задачи - выполняем свои
 	while (GetTask(&currTask))
-        {
-	        if(changeComm) ChangeCommunicator();
-             	currTask -> Run();
-        }
-	int  exitTask = 0;
-                
-	// Запрашиваем по одной задаче от каждого узла кроме самого себя
-	for (int i = 0 ; i < size || !serverFinished || changeComm; i++)
 	{
-		if(changeComm) ChangeCommunicator();
+		// Если происходит изменение коммуникатора
+		if (changeComm)
+		{
+			// И сообщение о смене коммуникатора ещё не отправлено
+			if (!message)
+			{
+				for (int i = 0; i < size; i++)
+				{
+					condition = 3;
+					// Отправка сообщения диспечеру со старым коммуникатором о том, 
+					// что коммуникатор сменён
+					MPI_Send(&condition, 1, MPI_INT, i, 1, Comm);
+					Comm = newComm;
+					message = true;
+				}
+			}
+		}
+		else message = false;
 
-		if (i != rank && i < size)
-		{	// Отправляем запрос на получение задачи (состояние здесь пока не причём
-			// в разработке)
-                        condition = 0;
-			MPI_Send(&condition, 1, MPI_INT, i, 1, currentComm);
+		currTask->Run();
+	}
+
+	// Запрашиваем по одной задаче от каждого узла кроме самого себя
+	for (int i = 0; i < size; i++)
+	{
+		// Если происходит изменение коммуникатора
+		if (changeComm)
+		{
+			// И сообщение о смене коммуникатора ещё не отправлено
+			if (!message)
+			{
+				for (int i = 0; i < size; i++)
+				{
+					condition = 3;
+					// Отправка сообщения диспечеру со старым коммуникатором о том, 
+					// что коммуникатор сменён
+					MPI_Send(&condition, 1, MPI_INT, i, 1, Comm);
+					Comm = newComm;
+					message = true;
+				}
+			}
+		}
+		else message = false;
+
+		if (i != rank)
+		{	// Отправляем запрос на получение задачи 
+			condition = 0;
+			MPI_Send(&condition, 1, MPI_INT, i, 1, Comm);
 			MPI_Status st;
 			// Получаем результат запроса в виде true или false
-			MPI_Recv(&exitTask, 1, MPI_INT, i, 2, currentComm, &st);
-									
+			MPI_Recv(&exitTask, 1, MPI_INT, i, 2, Comm, &st);
+
 			// Если узел готов отправить задачу на обработку
-			if (exitTask) 
-			{	
+			if (exitTask)
+			{
 				// Получаем данные задачи
 				int begin, end;
-				MPI_Recv(&begin, 1, MPI_INT, i, 3, currentComm, &st);
-				MPI_Recv(&end, 1, MPI_INT, i, 3, currentComm, &st);
+				MPI_Recv(&begin, 1, MPI_INT, i, 3, Comm, &st);
+				MPI_Recv(&end, 1, MPI_INT, i, 3, Comm, &st);
 				Task t(begin, end);
-				//int length;
-				//MPI_Recv(&length, 1, MPI_INT, i, 3, currentComm, &st);
-				//char *buf = new char(length);
-				//MPI_Recv(&buf, length, MPI_CHAR, i, 3, currentComm, &st);
-				//Task t(buf);
+
 				t.Run();
-				//delete[] buf;
-                                // Если задача от текущего узла была получена, берём задачи из
+
+				// Если задача от текущего узла была получена, берём задачи из
 				// этого узла до тех пор, пока задачи на нём не будут исчерпаны
 				i--;
 			}
 		}
-                else if(i >= size) i--;
 	}
 	// -//- процесс смог успешно завершиться
 	pthread_mutex_lock(&mutex);
 	std::cerr << "rank " << rank << ":: FINISHED THREAD\n";
 	pthread_mutex_unlock(&mutex);
-
+	return 0;
 }
-
-
-void* dispatcher(void* me)
+//Диспетчер для работы в старом коммуникаторе
+void* dispatcher_old(void* me)
 {
 	Task *t;
-	int cond; 
+	int cond;
 	int countOfAsks = 0;
 
-	// Пока не получены сообщения от макс кол-ва узлов (после завершения собственных задач процесса)
-	while(countOfAsks < (size-1)*countOfWorkers || !serverFinished || changeComm)
+	// Пока не получены сообщения о смене коммуникатора от макс кол-ва вычислительных потоков
+	while (count_change != rank_old * countOfWorkers)
 	{
 		MPI_Status st;
 		// Получаем запрос от любого узла
 		MPI_Recv(&cond, 1, MPI_INT, MPI_ANY_SOURCE, 1, currentComm, &st);
+
+		// Если это запрос о получении задачи
 		if (cond == 0)
 		{
 			// Получаем номер этого узла
 			int peer = st.MPI_SOURCE;
 			// Флаг ответа на то, есть задачи в узле, или нет
 			int send = 0;
+
+			// Если в очереди есть задача, получаем её
+			if (GetTask(&t))
+			{
+				int buf[2];
+				buf[0] = t->GetBegin();
+				buf[1] = t->GetEnd();
+				send = 1;
+				pthread_mutex_lock(&mutex);
+				std::cerr << "task send:: rank " << rank << " to " << peer << "\n";
+				pthread_mutex_unlock(&mutex);
+
+				// Отправляем сообщение о том, что можем отправить задачу и отправляем её
+				MPI_Send(&send, 1, MPI_INT, peer, 2, currentComm);
+				MPI_Send(&(buf), 1, MPI_INT, peer, 3, currentComm);
+				MPI_Send(&(buf)+1, 1, MPI_INT, peer, 3, currentComm);
+			}
+			// Собственные задачи кончились, запускаем счётчик ответов на все узлы
+			else if (countOfAsks < (size - 1)*countOfWorkers)
+			{
+				// Отправляем сообщение о том, что задачи кончились
+				MPI_Send(&send, 1, MPI_INT, peer, 2, currentComm);
+				countOfAsks++;
+			}
+		}
+		if (cond == 3)
+		{
+			count_change++;
+		}
+	}
+
+	currentComm = newComm;
+	changeComm = false;
+	return 0;
+}
+
+// Диспетчер, который работает всегда в новом коммуникаторе
+void* dispatcher(void* me)
+{
+	Task *t;
+	int cond;
+	int countOfAsks = 0;
+	MPI_Comm Comm = currentComm;
+
+	// Пока не получены запросы задач от макс кол-ва вычислительных потоков
+	while (countOfAsks < (size - 1)*countOfWorkers)
+	{
+		MPI_Status st;
+		// Получаем запрос от любого узла
+		MPI_Recv(&cond, 1, MPI_INT, MPI_ANY_SOURCE, 1, currentComm, &st);
+
+		// Если это запрос о получении задачи
+		if (cond == 0)
+		{
+			// Получаем номер этого узла
+			int peer = st.MPI_SOURCE;
+			// Флаг ответа на то, есть задачи в узле, или нет
+			int send = 0;
+
 			// Если в очереди есть задача, получаем её (тоже не отлажено)
 			if (GetTask(&t))
 			{
 				int buf[2];
-				buf[0] = t -> GetBegin();
-				buf[1] = t -> GetEnd();
+				buf[0] = t->GetBegin();
+				buf[1] = t->GetEnd();
 				send = 1;
+
 				pthread_mutex_lock(&mutex);
-				std:: cerr << "task send:: rank " << rank << " to " << peer << "\n"; 
+				std::cerr << "task send:: rank " << rank << " to " << peer << "\n";
 				pthread_mutex_unlock(&mutex);
+
 				// Отправляем сообщение о том, что можем отправить задачу и отправляем её
-				MPI_Send(&send, 1, MPI_INT, peer, 2, currentComm);	
-				MPI_Send(&(buf), 1, MPI_INT, peer, 3, currentComm);
-				MPI_Send(&(buf)+1, 1, MPI_INT, peer, 3, currentComm);
-		//			char *buf;
-		//			int length;
-		//			t->Serialize(&buf, &length);
-		//			MPI_Send(&length, 1, MPI_INT, peer, 3, currentComm);
-		//			MPI_Send(&buf, length, MPI_CHAR, peer, 3, currentComm);
-					
-					//Task.bufFree();
-		//			delete[] buf;
+				MPI_Send(&send, 1, MPI_INT, peer, 2, Comm);
+				MPI_Send(&(buf), 1, MPI_INT, peer, 3, Comm);
+				MPI_Send(&(buf)+1, 1, MPI_INT, peer, 3, Comm);
 			}
 			// Собственные задачи кончились, запускаем счётчик ответов на все узлы
-			else if (countOfAsks < (size-1)*countOfWorkers)
-			{	// Отправляем сообщение о том, что задачи кончились
-				MPI_Send(&send, 1, MPI_INT, peer, 2, currentComm);
+			else
+			{
+				// Отправляем сообщение о том, что задачи кончились
+				MPI_Send(&send, 1, MPI_INT, peer, 2, Comm);
 				countOfAsks++;
 			}
-		}		//break;
-		else if (cond == 1)
-		{      		        
-                        for (int j = 0; j < countOfWorkers - 1; j++)
-                                MPI_Recv(&cond, 1, MPI_INT, rank, 1, currentComm, &st);			                   
-        		currentComm = newComm;	
-                        MPI_Comm_rank(currentComm, &rank);
-                        MPI_Comm_size(currentComm, &size);
-			changeComm = false;
 		}
-                else serverFinished = true;
+		else if (cond == 1)
+		{
+			rank_old = rank;
+			size_old = size;
+
+			// Атрибуты потока
+			pthread_attr_t attrs;
+
+			// Инициализация атрибутов потока
+			if (0 != pthread_attr_init(&attrs))
+			{
+				perror("Cannot initialize attributes");
+				abort();
+			};
+
+			// Порождение диспетчера, работающего в старом коммуникаторе
+			if (0 != pthread_create(&thrs[countOfWorkers + 2], &attrs, dispatcher_old, &ids[countOfWorkers + 2]))
+			{
+				perror("Cannot create a thread");
+				abort();
+			}
+
+			//Меняем текущий коммуникатор на новый                  
+			Comm = newComm;
+
+			// Вычисляем новый размер и rаnk
+			MPI_Comm_rank(Comm, &rank);
+			MPI_Comm_size(Comm, &size);
+
+			changeComm = true;
+		}
 	}
 
 	// Сообщение об окончании работы диспетчера (отладка)
 	std::cerr << "rank " << rank << ":: despatcher closed.\n";
+	return 0;
 }
 
 void* server(void *me)
@@ -195,99 +337,111 @@ void* server(void *me)
 		for (int i = 0; i < MPI_MAX_PORT_NAME; i++)
 			fPort << port_name[i];
 		fPort.close();
-                 printf("server available at %s\n", port_name);
+		printf("server available at %s\n", port_name);
 	}
 
 	MPI_Comm_accept(port_name, MPI_INFO_NULL, 0, currentComm, &client);
-	again = 2;
-	
+	again = 1;
+
 	MPI_Intercomm_merge(client, false, &newComm);
-	if (size == 1) currentComm = newComm;
-	else changeComm = true;
-        MPI_Send (&again, 1, MPI_INT,rank, 1, currentComm); 
+
+	MPI_Send(&again, 1, MPI_INT, rank, 1, currentComm);
+	return 0;
 }
- 
+
 int main(int argc, char **argv)
 {
-	int tasksPerProcess = 5000000, fragmentSize;
+	int fragmentSize;
 	int lvl;
 	MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &lvl);
 	MPI_Comm_rank(currentComm, &rank);
 	MPI_Comm_size(currentComm, &size);
 	MPI_Status st;
-        
-//      std::cerr << "level" << lvl << "\n";
-	fragmentSize = globalTaskSize /(size*tasksPerProcess);
 
-        int status = pthread_barrier_init(&barrier, NULL, countOfWorkers);
-        if (status != 0) 
-        {
-                printf("main error: can't init barrier, status = %d\n", status);
-                exit(ERROR_INIT_BARRIER);
-        }
+	//      std::cerr << "level" << lvl << "\n";
+	fragmentSize = globalTaskSize / (size*tasksPerProcess);
 
-	// Формирование очереди
+	int status = pthread_barrier_init(&barrier, NULL, countOfWorkers);
+	if (status != 0)
+	{
+		printf("main error: can't init barrier, status = %d\n", status);
+		exit(ERROR_INIT_BARRIER);
+	}
+
+	// Инициализация мьютекса
+	pthread_mutexattr_t attr;
+	pthread_mutexattr_init(&attr);
+	pthread_mutex_init(&mutex, &attr);
+
+	// Формирование очереди задач
+	int begin = rank * fragmentSize*tasksPerProcess;
 	for (int i = 0; i < tasksPerProcess; i++)
 	{
-		int begin = rank*fragmentSize*tasksPerProcess;
-		tasksContainer.push(new Task(begin, begin + fragmentSize));
+		QueueOfTasks.push(new Task(begin, begin + fragmentSize));
+		begin += fragmentSize;
 	}
-//	if (rank == 0)
-//	        for(int i = 0; i < globalTaskSize; i++)	
-//		        tasksContainer.push(new Task(i, i+1));
-///
-	for (int i = 0; i < globalTaskSize; i++) 
+	//	if (rank == 0)
+	//	        for(int i = 0; i < globalTaskSize; i++)	
+	//		        QueueOfTasks.push(new Task(i, i+1));
+	///
+	for (int i = 0; i < globalTaskSize; i++)
 		v1[i] = v2[i] = 1;
-	
-    //атрибуты потока
+
+	// Атрибуты потока
 	pthread_attr_t attrs;
- 
-    //инициализация атрибутов потока
-	if(0!=pthread_attr_init(&attrs))	
+
+	// Инициализация атрибутов потока
+	if (0 != pthread_attr_init(&attrs))
 	{
-   		perror("Cannot initialize attributes");
-    		abort();
+		perror("Cannot initialize attributes");
+		abort();
 	};
-    //установка атрибута "присоединенный"
-	if(0!=pthread_attr_setdetachstate(&attrs, PTHREAD_CREATE_JOINABLE))
+	// Установка атрибута "присоединенный"
+	if (0 != pthread_attr_setdetachstate(&attrs, PTHREAD_CREATE_JOINABLE))
 	{
-    		perror("Error in setting attributes");
-    		abort();
-    	}
-    //порождение четырех потоков
-    	for(int i = 0; i < countOfWorkers; i++)
-    	if(0!=pthread_create(&thrs[i], &attrs, worker, &ids[i]))
-    	{
-    		perror("Cannot create a thread");
-    		abort();
-    	}
-
-	if(0!=pthread_create(&thrs[1], &attrs, dispatcher, &ids[1]))
-        {
-                perror("Cannot create a thread");
-                abort();
-        }
-
-	if(0!=pthread_create(&thrs[2], &attrs, server, &ids[2]))
-        {
-                perror("Cannot create a thread");
-                abort();
-        }
-
-    //освобождение ресурсов, занимаемых описателем атрибутов
-    	pthread_attr_destroy(&attrs);
-    //ожидание завершения порожденных потоков
-   	for(int i = 0; i < countOfThreads; i++)
-	   	if(0!=pthread_join(thrs[i], NULL))
+		perror("Error in setting attributes");
+		abort();
+	}
+	// Порождение вычислительных потоков
+	for (int i = 0; i < countOfWorkers; i++)
+		if (0 != pthread_create(&thrs[i], &attrs, worker, &ids[i]))
 		{
-    			perror("Cannot join a thread");
-   	 		abort();
+			perror("Cannot create a thread");
+			abort();
 		}
-        std:: cerr << "threads finished\n";
+
+	// Порождение диспетчера
+	if (0 != pthread_create(&thrs[countOfWorkers], &attrs, dispatcher, &ids[countOfWorkers]))
+	{
+		perror("Cannot create a thread");
+		abort();
+	}
+
+	// Порождение сервера
+	/*if (0 != pthread_create(&thrs[countOfWorkers + 1], &attrs, server, &ids[countOfWorkers + 1]))
+	{
+		perror("Cannot create a thread");
+		abort();
+	}*/
+
+	//освобождение ресурсов, занимаемых описателем атрибутов
+	pthread_attr_destroy(&attrs);
+	pthread_mutexattr_destroy(&attr);
+
+	//ожидание завершения порожденных потоков
+	for (int i = 0; i < countOfThreads-1; i++)
+		if (0 != pthread_join(thrs[i], NULL))
+		{
+			perror("Cannot join a thread");
+			abort();
+		}
+	std::cerr << "threads finished\n";
+
 	MPI_Reduce(&globalRes, &resultOfGlobalTask, 1, MPI_INT, MPI_SUM, 0, currentComm);
+
 	if (rank == 0)
 		std::cerr << "Global result = " << resultOfGlobalTask << "\n";
-	MPI_Finalize();	
+	MPI_Finalize();
+
 	return 0;
 }
-
